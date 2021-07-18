1af23d91d30bac11229a1ddaa29f8276f5e4440c
Philipp Ochs <philipp@famochs.de>
2021-07-18 10:59:59 +0200

    Added few messages.
    Now goin brrr in two languages.

--- src/main/java/kit/pse/hgv/graphSystem/GraphSystemMessages.java
+++ src/main/java/kit/pse/hgv/graphSystem/GraphSystemMessages.java
@@ -1,13 +1,21 @@
 package kit.pse.hgv.graphSystem;
 
 public enum GraphSystemMessages {
-    TO_MANY_ELEMENTS("Es wurden zu viele GraphElemente erstellt");
+    TO_MANY_ELEMENTS("Es wurden zu viele GraphElemente erstellt!", "There are to much elements existing!"),
+    EDGE_ONLY_WITH_NODES("Eine Kante kann nur mit Knoten verbunden werden!", "An edge can only be connected with nodes!"),
+    MAX_NODES_EDGE("Eine Kante kann nur mit 2 Knoten verbunden werden!", "An edge can only be connected with two nodes!"),
+    PATH_ERROR("Der Pfad ist ungültig oder das Format wurde nicht erkannt!", "The path is not existing or the format is not supportet!");
 
-    private String mes;
-    private GraphSystemMessages(String mes) {
-        this.mes = mes;
+    private final String DE;
+    private final String EN;
+    private GraphSystemMessages(String de, String en) {
+        EN = en;
+        this.DE = de;
     }
-    public String getMessage() {
-        return mes;
+    public String DE() {
+        return DE;
+    }
+    public String EN() {
+        return EN;
     }
 }
