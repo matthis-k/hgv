033211af36fc6c6c11e79206949e2f35a430f6fc
Chrissy Wenzel <Christina_Regina@hotmail.de>
2021-07-19 15:38:59 +0200

    Add comments and let the process method return enum for the extension

--- src/main/java/kit/pse/hgv/commandProcessor/ExtensionCommandInput.java
+++ src/main/java/kit/pse/hgv/commandProcessor/ExtensionCommandInput.java
@@ -1,87 +1,159 @@
 package kit.pse.hgv.commandProcessor;
 
-import kit.pse.hgv.commandController.commands.Command;
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.json.JSONObject;
+import org.json.JSONException;
 
+/**
+ * This class processes the string command from the extension server and delegates the command to the command controller.
+ */
 public enum ExtensionCommandInput {
 
-    CREATE_NODE(ExtensionCommandInput.START + "create node " + ExtensionCommandInput.GRAPHID + ExtensionCommandInput.COORDINATE + ExtensionCommandInput.END){
+    /**
+     * This enum handles the command string that creates nodes.
+     */
+    CREATE_NODE(ExtensionCommandInput.START + "CreateNode" + ExtensionCommandInput.END){
         @Override
-        protected void parseCommand(Matcher matcher, Command command) {
-            int GraphID;
+        protected void parseCommand(JSONObject jsonExtensionInput) {
+            String coordinate = jsonExtensionInput.getString("coordinate");
+            coordinate = coordinate.replaceAll("[\\p{Ps}\\p{Pe}]", "");
+            //String[] eachCoordinate = coordinate.split(",");
+            //Woher weiß man bei (2,1,0) welche x Koordinate zu welcher y Koordinate gehört? Sollte man das in (2,1;0) angeben oder so?
+        }
+    },
+
+
+    /**
+     * This enum handles the command string that creates edges.
+     */
+    CREATE_EDGE(ExtensionCommandInput.START + "CreateEdge" + ExtensionCommandInput.END){
+        @Override
+        protected void parseCommand(JSONObject jsonExtensionInput) {
+            int firstNode = jsonExtensionInput.getInt("id1");
+            int secondNode = jsonExtensionInput.getInt("id2");
+        }
+    },
+
+    /**
+     * This enum handles the command string that deletes nodes.
+     */
+    DELETE_NODE(ExtensionCommandInput.START + "DeleteNode" + ExtensionCommandInput.END){
+        @Override
+        protected void parseCommand(JSONObject jsonExtensionInput) {
+            int nodeID = jsonExtensionInput.getInt("node_id");
+        }
+    },
+
+    /**
+     * This enum handles the command string that deletes edges.
+     */
+    DELETE_EDGE(ExtensionCommandInput.START + "DeleteEdge" + ExtensionCommandInput.END) {
+        @Override
+        protected void parseCommand(JSONObject jsonExtensionInput) {
+            int edgeID = jsonExtensionInput.getInt("edge_id");
+        }
+    },
+
+    /**
+     * This enum handles the command string that moves nodes.
+     */
+    MOVE_NODE(ExtensionCommandInput.START + "MoveNode" + ExtensionCommandInput.END){
+        @Override
+        protected void parseCommand(JSONObject jsonExtensionInput) {
 
         }
     },
 
-    CREATE_EDGE(ExtensionCommandInput.START + "create edge " + ExtensionCommandInput.GRAPHID + ExtensionCommandInput.END){
+    /**
+     * This enum handles the command string that changes the color.
+     */
+    CHANGE_COLOR(ExtensionCommandInput.START + "ChangeColor" + ExtensionCommandInput.END){
         @Override
-        protected void parseCommand(Matcher matcher, Command command) {
-            int GraphID;
+        protected void parseCommand(JSONObject jsonExtensionInput) {
+            int elementID = jsonExtensionInput.getInt("elementId");
+            //Hexazahl
         }
     },
 
-    DELETE_NODE(ExtensionCommandInput.START + ExtensionCommandInput.END){
+    /**
+     * FRAGE: Machen wir einen ChangeMetadata Befehl, der nochmal einen extra Typ hat oder spalten wir das in einzelne Befehle auf?
+     */
+    CHANGE_METADATA(ExtensionCommandInput.START + "ChangeMetadata" + ExtensionCommandInput.END){
         @Override
-        protected void parseCommand(Matcher matcher, Command command) {
-
-        }
-    },
-
-    DELETE_EDGE(ExtensionCommandInput.START + ExtensionCommandInput.END) {
-        @Override
-        protected void parseCommand(Matcher matcher, Command command) {
-
-        }
-    },
-
-    MOVE_NODE(ExtensionCommandInput.START + ExtensionCommandInput.END){
-        @Override
-        protected void parseCommand(Matcher matcher, Command command) {
+        protected void parseCommand(JSONObject jsonExtensionInput) {
 
         }
     },
 
-    CHANGE_COLOR(ExtensionCommandInput.START + ExtensionCommandInput.COLOR + ExtensionCommandInput.END){
+    /**
+     * This enum handles the command string that moves the center of the presentation.
+     */
+    MOVE_CENTER(ExtensionCommandInput.START + "MoveCenter" + ExtensionCommandInput.END){
         @Override
-        protected void parseCommand(Matcher matcher, Command command) {
+        protected void parseCommand(JSONObject jsonExtensionInput) {
 
         }
     },
 
-    MOVE_CENTER(ExtensionCommandInput.START + ExtensionCommandInput.END){
+    /**
+     * This enum handles the command string that pauses the script.
+     */
+    PAUSE(ExtensionCommandInput.START + "Pause" + ExtensionCommandInput.END){
         @Override
-        protected void parseCommand(Matcher matcher, Command command) {
+        protected void parseCommand(JSONObject jsonExtensionInput) {
 
         }
-    }
-
-    ;
+    };
 
     private static final String START = "^";
     private static final String END = "$";
-    private static final String GRAPHID = "[0-9]+";
-    private static final String COORDINATE = "\\([0-9]+\\,[0-9]+\\)";
-    private static final String COLOR = "#([a-fA-F0-9]{6})";
 
     private final Pattern pattern;
 
+    /**
+     * The constructor creates an element of this class
+     *
+     * @param s the pattern associated to the input
+     */
     ExtensionCommandInput(String s) {
         this.pattern = Pattern.compile(s);
     }
 
-    public static void processCommandString(String extensionInput){
+    /**
+     * This method decides whether the command from the extension is in the correct format
+     *
+     * @param extensionInput the command as string from the extension
+     * @return the command as enum
+     */
+    public static ExtensionCommandInput processCommandString(String extensionInput){
         boolean correctInput = false;
-        for (ExtensionCommandInput input : ExtensionCommandInput.values()) {
-            Matcher matcher = input.pattern.matcher(extensionInput);
-            correctInput = true;
-        }
-        if (!correctInput) {
-            //AN CONTROLLER FEHLERNACHRICHT GEBEN UND STOPP DES SKRIPTS BEANTRAGEN
+        ExtensionCommandInput enumInput = null;
+        try {
+            JSONObject jsonExtensionInput = new JSONObject(extensionInput);
+            for (ExtensionCommandInput input : ExtensionCommandInput.values()) {
+                Matcher matcher = input.pattern.matcher((String) jsonExtensionInput.get("type"));
+                if(matcher.matches()) {
+                    input.parseCommand(jsonExtensionInput);
+                    enumInput = input;
+                    correctInput = true;
+                }
+            }
+            if (correctInput) {
+                //AN CONTROLLER FEHLERNACHRICHT GEBEN UND STOPP DES SKRIPTS BEANTRAGEN
+                return enumInput;
+            } else {
+                throw new IllegalArgumentException("Command Type is non-existent.");
+            }
+        } catch (JSONException e) {
             throw new IllegalArgumentException("Command is not in the correct format.");
         }
     }
 
-    protected abstract void parseCommand(Matcher matcher, Command command);
+    /**
+     * This method decides whether the parameters from the given string from the extension are correct
+     *
+     * @param jsonExtensionInput String as JSON Object
+     */
+    protected abstract void parseCommand(JSONObject jsonExtensionInput);
 }
--- src/main/java/kit/pse/hgv/commandProcessor/ExtensionCommandProcessor.java
+++ src/main/java/kit/pse/hgv/commandProcessor/ExtensionCommandProcessor.java
@@ -1,18 +1,28 @@
 package kit.pse.hgv.commandProcessor;
 import kit.pse.hgv.commandController.commands.*;
 
+/**
+ * This class processes the commands coming from the extension. It queues the command in the command controller, if it is in the correct format.
+ */
+
 public class ExtensionCommandProcessor implements CommandProcessor{
     @Override
     public void queueCommand(Command command) {
 
     }
 
-    public void getCommand(String extensionInput) {
-        Command command;
+    /**
+     * This method receives the command from the extension as a string and returns an enum as command
+     *
+     * @param extensionInput the command as string from the extension
+     * @return the command as enum
+     */
+    public ExtensionCommandInput processCommand(String extensionInput) {
         try {
-            ExtensionCommandInput.processCommandString(extensionInput);
+            return ExtensionCommandInput.processCommandString(extensionInput);
         } catch (IllegalArgumentException e) {
-            //ERROR MESSAGE
+            //TODO
+            return null;
         }
     }
 }
