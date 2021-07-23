dd81e282d02a55c0e8e5b635283a966281dab36a
Matthis Kälble <matthis.kaelble@gmail.com>
2021-07-23 12:30:18 +0200

    fix: tests now work

--- src/test/java/com/hgv/graphSystem/GraphTest.java
+++ src/test/java/com/hgv/graphSystem/GraphTest.java
@@ -7,11 +7,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+
 import static org.junit.Assert.*;
 
 public class GraphTest {
 
     private GraphSystem g;
+    private int graphId;
     @Before
     public void init() {
         g = GraphSystem.getInstance();
@@ -20,53 +22,50 @@ public class GraphTest {
 
     @Test
     public void accessGraph() {
-        g.loadGraph("");
-        assertNotNull(g.getGraphElementByID(1));
+        graphId = g.loadGraph("");
+        assertNotNull(g.getGraphByID(graphId));
     }
 
     @Test
     public void addNode() throws OverflowException {
-        g.loadGraph("");
-        g.addElement(1, new Coordinate());
-        assertNotNull(g.getGraphElementByID(1));
+        graphId = g.loadGraph("");
+        int id = g.addElement(graphId, new Coordinate());
+        assertNotNull(g.getGraphElementByID(id));
     }
 
     @Test
     public void addEdge() throws OverflowException {
-        g.loadGraph("");
-        g.addElement(1, new Coordinate());
-        g.addElement(1, new Coordinate());
-        int[] nodes = {1,2};
-        g.addElement(1, nodes);
+        graphId = g.loadGraph("");
+        int id1 = g.addElement(graphId, new Coordinate());
+        int id2 = g.addElement(graphId, new Coordinate());
+        int[] nodes = {id1, id2};
+        int id3 = g.addElement(graphId, nodes);
 
-        assertNotNull(g.getGraphElementByID(3));
+        assertNotNull(g.getGraphElementByID(id3));
     }
 
     @Test
     public void removeElement() throws OverflowException {
-        g.loadGraph("");
-        g.addElement(1, new Coordinate());
-        g.removeElement(1);
-        assertNull(g.getGraphElementByID(1));
+        graphId = g.loadGraph("");
+        int id = g.addElement(graphId, new Coordinate());
+        g.removeElement(id);
+        assertNull(g.getGraphElementByID(id));
     }
 
     @Test
     public void removeNodeDelEdges() throws OverflowException {
-        g.loadGraph("");
-        int id1 = g.addElement(1, new Coordinate());
-        int id2 = g.addElement(1, new Coordinate());
-        System.out.println(id1);
-        System.out.println(id2);
+        graphId = g.loadGraph("");
+        int id1 = g.addElement(graphId, new Coordinate());
+        int id2 = g.addElement(graphId, new Coordinate());
         int[] nodes = {id1, id2};
-        int id3 = g.addElement(1, nodes);
-        System.out.println(id3);
+        int id3 = g.addElement(graphId, nodes);
         g.removeElement(id2);
-        System.out.println(g.getGraphElementByID(3));
-        assertNull(g.getGraphElementByID(3));
+        assertNull(g.getGraphElementByID(id3));
     }
 
     @After
     public void destroy() {
-        g.removeGraph(0);
+        g.removeGraph(graphId);
+        graphId = 0;
     }
 }
