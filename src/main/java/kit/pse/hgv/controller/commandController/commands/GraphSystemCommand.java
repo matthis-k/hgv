a81a782de088158a1a0e9a51886d73a58cd26c9c
Philipp Ochs <philipp@famochs.de>
2021-08-14 15:15:47 +0200

    Feature: workingArea GraphSysCommand

--- src/main/java/kit/pse/hgv/controller/commandController/commands/GraphSystemCommand.java
+++ src/main/java/kit/pse/hgv/controller/commandController/commands/GraphSystemCommand.java
@@ -1,10 +1,19 @@
 package kit.pse.hgv.controller.commandController.commands;
 
+import kit.pse.hgv.graphSystem.element.GraphElement;
+
+import java.util.List;
+
 /**
  * This class handles all Commands managing the graphSystem
  */
 public abstract class GraphSystemCommand extends Command {
     protected int graphId;
+
+    /** Elements which will be changed in execution. */
+    private List<GraphElement> workingArea = new List<GraphElement>();
+
+
     /**
      * The constructor creates an element of this class with a specific graphId
      * 
@@ -18,5 +27,9 @@ public abstract class GraphSystemCommand extends Command {
      * The constructor creates an element of this class without a specific graph
      */
     public GraphSystemCommand() {
+    }
+
+    public List<GraphElement> getWorkingArea() {
+        return workingArea;
     }
 }
