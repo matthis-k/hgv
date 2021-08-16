16dbc516c6d2225ef215f25b4a230bcc0cd82f70
Matthis Kälble <matthis.kaelble@gmail.com>
2021-08-16 08:20:07 +0200

    test: ExtensionServer now has full coverage.

--- src/test/java/kit/pse/hgv/extensionServer/ExtensionServerTest.java
+++ src/test/java/kit/pse/hgv/extensionServer/ExtensionServerTest.java
@@ -5,14 +5,25 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import kit.pse.hgv.controller.commandController.CommandController;
+import kit.pse.hgv.controller.commandController.CommandEventSource;
+import kit.pse.hgv.controller.commandController.CommandQListener;
+import kit.pse.hgv.controller.commandController.commands.ICommand;
+import kit.pse.hgv.controller.commandController.commands.SendGraphCommand;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.ref.Cleaner.Cleanable;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
+import java.util.Vector;
 
 public class ExtensionServerTest {
     private static ExtensionServer server = ExtensionServer.getInstance();
@@ -62,6 +73,41 @@ public class ExtensionServerTest {
         server.pause(8);
         server.stop(8);
         server.send(8, "");
+    }
+
+    @Test(expected = SocketTimeoutException.class)
+    public void onNotify() throws UnknownHostException, IOException {
+        CommandEventSource emitter = new CommandEventSource() {
+            private Vector<CommandQListener> listeners = new Vector<>();
+
+            @Override
+            public void register(CommandQListener listener) {
+                listeners.add(listener);
+            }
+
+            @Override
+            public void notifyAll(ICommand c) {
+                for (CommandQListener listener : listeners) {
+                    listener.onNotify(c);
+                }
+            }
+        };
+        emitter.register(server);
+        Socket client = new Socket("localhost", server.getPort());
+        client.setSoTimeout(100);
+        ICommand c = new SendGraphCommand(1);
+        c.setClientId(1);
+        emitter.notifyAll(c);
+        sleep(100);
+        try {
+            String received = new BufferedReader(new InputStreamReader(client.getInputStream())).readLine();
+            assertEquals("{}", received);
+        } catch (SocketTimeoutException e) {
+            assertTrue("timeout ocuured", false);
+        }
+        c.setClientId(0);
+        emitter.notifyAll(c);
+        String received = new BufferedReader(new InputStreamReader(client.getInputStream())).readLine();
     }
 
     @After
