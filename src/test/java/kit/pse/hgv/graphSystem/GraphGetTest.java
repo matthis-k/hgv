c2274d07d3bc1af076cf7df35dc3c2e05b30de66
Philipp Ochs <philipp@famochs.de>
2021-08-23 13:35:27 +0200

    test: getNodeById / getEdgeById (getElementById)

---
+++ src/test/java/kit/pse/hgv/graphSystem/GraphGetTest.java
@@ -0,0 +1,62 @@
+package kit.pse.hgv.graphSystem;
+
+import kit.pse.hgv.graphSystem.element.Edge;
+import kit.pse.hgv.graphSystem.element.GraphElement;
+import kit.pse.hgv.graphSystem.element.Node;
+import kit.pse.hgv.graphSystem.exception.OverflowException;
+import kit.pse.hgv.representation.CartesianCoordinate;
+import kit.pse.hgv.representation.Coordinate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class GraphGetTest {
+
+    private GraphSystem g;
+    private int graphId;
+    @Before
+    public void init() {
+        g = GraphSystem.getInstance();
+    }
+
+    @After
+    public void destroy() {
+        g.removeGraph(graphId);
+        graphId = 0;
+    }
+
+    @Test
+    public void getNodeById() throws OverflowException {
+        Coordinate c = new CartesianCoordinate(0, 0);
+        g.newGraph();
+        g.addElement(1, c);
+        assert(g.getGraphElementByID(1).getId() == 1);
+        assert(((Node) g.getGraphElementByID(1)).getCoord().equals(c));
+    }
+
+    @Test
+    public void getEdgeById() throws OverflowException {
+        Coordinate c0 = new CartesianCoordinate(0, 0);
+        Coordinate c1 = new CartesianCoordinate(1, 1);
+        g.newGraph();
+        g.addElement(1, c0);
+        g.addElement(1, c1);
+        int[] nds = {1,2};
+        g.addElement(1, nds);
+        assert(g.getGraphElementByID(3).getId() == 3);
+        assert(((Edge) g.getGraphElementByID(3)).getNodes()[0].getId() == 1);
+        assert(((Edge) g.getGraphElementByID(3)).getNodes()[1].getId() == 2);
+    }
+
+
+
+
+
+
+
+
+
+
+}
