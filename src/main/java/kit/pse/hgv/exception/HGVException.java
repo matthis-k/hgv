f5b1f5808da870b06ae8b33ac669908db09ee172
Philipp Ochs <philipp@famochs.de>
2021-09-04 12:48:30 +0200

    fix: now looking more sexy

--- src/main/java/kit/pse/hgv/exception/HGVException.java
+++ src/main/java/kit/pse/hgv/exception/HGVException.java
@@ -3,5 +3,4 @@ package kit.pse.hgv.exception;
 /**
  * This exception is the HGV-wide Exception which covers all exceptions that can happen in the HGV by purpose.
  */
-public class HGVException extends Exception {
-}
+public class HGVException extends Exception {}
