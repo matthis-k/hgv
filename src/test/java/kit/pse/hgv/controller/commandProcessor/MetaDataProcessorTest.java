39ce813e312da60638f22060c0ec7e27be8b7312
Chrissy Wenzel <Christina_Regina@hotmail.de>
2021-09-07 22:06:48 +0200

    Edit CommandProcessor and add tests

--- src/test/java/kit/pse/hgv/controller/commandProcessor/MetaDataProcessorTest.java
+++ src/test/java/kit/pse/hgv/controller/commandProcessor/MetaDataProcessorTest.java
@@ -3,8 +3,11 @@ package kit.pse.hgv.controller.commandProcessor;
 import static org.junit.Assert.assertTrue;
 
 import kit.pse.hgv.controller.commandController.CommandController;
+import kit.pse.hgv.controller.commandController.commands.CommandComposite;
 import kit.pse.hgv.controller.commandController.commands.EditUserMetaCommand;
 import org.junit.*;
+
+import java.util.HashMap;
 
 public class MetaDataProcessorTest {
 
@@ -24,6 +27,15 @@ public class MetaDataProcessorTest {
     public void testChangeMetadata() {
         metaDataProcessor.editMetaData(1, "color", "red");
         assertTrue(CommandController.getInstance().getCommandQ().poll() instanceof EditUserMetaCommand);
+    }
+
+    @Test
+    public void testChangeMetadataMap() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("color", "red");
+        map.put("r", "2");
+        metaDataProcessor.editMetaData(1, map);
+        assertTrue(CommandController.getInstance().getCommandQ().poll() instanceof CommandComposite);
     }
 
     @After
